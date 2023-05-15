50e0a935d8a6dcee48715fd94bd94b44501d3791
George H. de Menibus <g.f.l.hellouindemenibus@student.tudelft.nl>
2023-05-15 15:12:35 +0200

    FEAT: Add floyd warshall from stack overflow

---
+++ src/floyd_warshall.rs
@@ -0,0 +1,21 @@
+fn floyd_warshall_fast(dist: &mut [Vec<i32>]) {
+    let n = dist.len();
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            let (dist_j, dist_i) = if j < i {
+                let (lo, hi) = dist.split_at_mut(i);
+                (&mut lo[j][..n], &mut hi[0][..n])
+            } else {
+                let (lo, hi) = dist.split_at_mut(j);
+                (&mut hi[0][..n], &mut lo[i][..n])
+            };
+            let dist_ji = dist_j[i];
+            for k in 0..n {
+                dist_j[k] = min(dist_j[k], dist_ji + dist_i[k]);
+            }
+        }
+    }
+}
