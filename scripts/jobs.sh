6beb4ac2bf2fa9772a5d8988d45e3711a0b63995
George H. de Menibus <g.f.l.hellouindemenibus@student.tudelft.nl>
2023-06-08 15:08:22 +0200

    FEAT: Script for parallel job processing

---
+++ scripts/jobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Directory containing the files
+directory="../data/datasets/j30.sm"
+
+# Function to process a file
+process_file() {
+    file="$1"
+    # Run your command with the file as an argument
+	../target/release/modeling "$file"
+}
+
+# Export the function to make it available to child processes
+export -f process_file
+
+# Generate the file list using find and run the command in parallel
+find "$directory" -type f -print0 | xargs -0 -n 1 -P 10 bash -c 'process_file "$@"' _
+
+# Note: Adjust the value of `-P` according to the number of parallel processes you want to use.
